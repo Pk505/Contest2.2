e81ae9065afb67088381b8751b7cd8943caabbb0
Pyotr K. <kordonpu@mail.ru>
2024-11-22 18:47:49 +0300

    updated .gitignore, form_table

--- main.py
+++ main.py
@@ -83,6 +83,7 @@ def merge(left_merge_tree, right_merge_tree):
         right_merge_tree.root.parent = left_merge_tree.root
         return left_merge_tree.root
 
+
 class DBT:
     def __init__(self, root = None):
         self.root = root
@@ -141,12 +142,13 @@ class DBT:
         if self.root is None:
             output = '_'
         else:
+
             self.form_table()
             for level in range(len(self.table)):
                 current_index = -1
                 level_size = 2 ** level
                 for vertex in self.table[level]:
-                    output += "_ " * (vertex[0] - 1 - current_index) + vertex[1]
+                    output += "_ " * (vertex[0] - 1 - current_index) + vertex[1].info()
                     current_index = vertex[0]
                     if current_index + 1 < level_size:
                         output += ' '
@@ -237,14 +239,12 @@ class DBT:
         self.table.clear()
         level = 0
         current_vertex = self.root
-        stack = [current_vertex]
-        while len(stack) > 0:
-            current_vertex = stack[-1]
+        while True:
 
-            if len(self.table) < level + 1:
+            if len(self.table) < level + 1:  # increasing tables size
                 self.table.append([])
 
-            if current_vertex.is_visited == self.is_table_passed:
+            if current_vertex.is_visited == self.is_table_passed:  #adding vertex in table
                 if level == 0:
                     current_vertex.index = 0
                 else:
@@ -252,19 +252,22 @@ class DBT:
                         current_vertex.index = current_vertex.parent.index * 2
                     else:
                         current_vertex.index = current_vertex.parent.index * 2 + 1
-                self.table[level].append([current_vertex.index, current_vertex.info()])
+                self.table[level].append([current_vertex.index, current_vertex])
 
                 current_vertex.is_visited = not current_vertex.is_visited
 
-            if current_vertex.left is not None and current_vertex.left.is_visited == self.is_table_passed:
-                stack.append(current_vertex.left)
+            if current_vertex.left is not None and current_vertex.left.is_visited == self.is_table_passed:  # tree's passage
                 level += 1
+                current_vertex=current_vertex.left
             elif current_vertex.right is not None and current_vertex.right.is_visited == self.is_table_passed:
-                stack.append(current_vertex.right)
                 level += 1
+                current_vertex=current_vertex.right
             else:
-                stack.pop()
                 level -= 1
+                if current_vertex != self.root:
+                    current_vertex = current_vertex.parent
+                else:
+                    break
 
 
         self.is_table_passed = not self.is_table_passed
@@ -311,57 +314,61 @@ class DBT:
                 rotate(splay_vertex)     # "zig-zag"
                 rotate(splay_vertex)
 
+
     def split(self,  split_vertex):
         self.splay(split_vertex)
         return  Pair(split_vertex, split_vertex.right)
 
 
 
-
-
-input_tree =  DBT()
-while True:
-    try:
-        command_string = input()
-        if len(command_string) == 0:
+def main():
+    input_tree = DBT()
+    while True:
+        try:
+            command_string = input()
+            if len(command_string) == 0:
+                break
+            command_info = command_string.split()
+            match command_info[0]:
+                case 'add':
+                    try:
+                        input_tree.add(int(command_info[1]), command_info[2])
+                    except AddException:
+                        print("error")
+                case 'set':
+                    try:
+                        input_tree.set(int(command_info[1]), command_info[2])
+                    except SetException:
+                        print("error")
+                case 'delete':
+                    try:
+                        input_tree.delete(int(command_info[1]))
+                    except DeleteException:
+                        print("error")
+                case 'search':
+                    try:
+                        found_value = input_tree.search(int(command_info[1]))
+                        print(f'1 {found_value}')
+                    except SearchException:
+                        print('0')
+                case 'min':
+                    try:
+                        min_vertex = input_tree.min()
+                        print(f'{min_vertex.key} {min_vertex.value}')
+                    except MinMaxException:
+                        print("error")
+                case 'max':
+                    try:
+                        max_vertex = input_tree.max()
+                        print(f'{max_vertex.key} {max_vertex.value}')
+                    except MinMaxException:
+                        print("error")
+                case 'print':
+                    print(input_tree.print(), end='')
+        except EOFError:
             break
-        command_info = command_string.split()
-        match command_info[0]:
-            case 'add':
-                try:
-                    input_tree.add(int(command_info[1]), command_info[2])
-                except AddException:
-                    print("error")
-            case 'set':
-                try:
-                    input_tree.set(int(command_info[1]), command_info[2])
-                except SetException:
-                    print("error")
-            case 'delete':
-                try:
-                    input_tree.delete(int(command_info[1]))
-                except DeleteException:
-                    print("error")
-            case 'search':
-                try:
-                    found_value = input_tree.search(int(command_info[1]))
-                    print(f'1 {found_value}')
-                except SearchException:
-                    print('0')
-            case 'min':
-                try:
-                    min_vertex = input_tree.min()
-                    print(f'{min_vertex.key} {min_vertex.value}')
-                except MinMaxException:
-                    print("error")
-            case 'max':
-                try:
-                    max_vertex = input_tree.max()
-                    print(f'{max_vertex.key} {max_vertex.value}')
-                except MinMaxException:
-                    print("error")
-            case 'print':
-                print(input_tree.print(), end='')
-    except EOFError:
-        break
+
+
+if __name__ == '__main__':
+   main()
 
